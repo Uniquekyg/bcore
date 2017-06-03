6638f73ec8eb68d61ef448c09bca0ad95f51d0b5
Konstantin Kim <uniquekyg@gmail.com>
2017-06-03 16:59:51 +0600

    add html

--- bcore/settings.py
+++ bcore/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '+ugn$mx3j^#q6$nsjr#45a)6lhmegd5&el5h2sxochrtbsb$3f'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'core',
 ]
 
 MIDDLEWARE = [
